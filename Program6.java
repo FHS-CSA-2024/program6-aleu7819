61e4521178d3769f097b075d74d3fbacb2eea90e
aleu7819 <allison.asl.leung@gmail.com>
2024-08-18 20:54:07 -0700

    Update Program6.java
    added variable names and print statements

--- Program6.java
+++ Program6.java
@@ -2,6 +2,31 @@
 
 //Your code here
 
+public class Program6
+{public static void main (String[] args)
+    {
+    
+//use final for pi value
+
+//get radius from user using a double
+
+//calculations
+        double circumference=0;
+        double area=0;
+        double radius=0;
+        double diameter=0;
+        
+        
+        System.out.println("the radius of the circle= " + radius);
+         System.out.println("the area of the circle= " + area);
+          System.out.println("the circumference of the circle= " + circumference);
+           System.out.println("the diameter of the circle= " + diameter);
+        
+    
+}}
+
+
+
 //Paste console output below:
 /*
 
