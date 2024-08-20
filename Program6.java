4a12f98818299ed5220d39918b6afcf3781b8d37
aleu7819 <allison.asl.leung@gmail.com>
2024-08-19 19:14:10 -0700

    Update Program6.java
    added equations and initialized values

--- Program6.java
+++ Program6.java
@@ -1,21 +1,32 @@
 //import stuff here!
 
 //Your code here
+import java.util.Scanner;
 
 public class Program6
 {public static void main (String[] args)
     {
     
-//use final for pi value
-
 //get radius from user using a double
+Scanner user_imput = new Scanner(System.in); 
+System.out.println("enter radius: ");
+double radius = user_imput.nextDouble();
+
+//use final for pi value
+final double piValue= 3.14159;
+
 
 //calculations
-        double circumference=0;
-        double area=0;
-        double radius=0;
-        double diameter=0;
+double diameter= radius*2;
         
+double circumference= 2*(piValue*radius);
+double area= piValue*(radius*radius);
+        
+//changing the decimal places
+radius= ((int)((radius*1000)+0.5))/1000.0;
+area= ((int)((area*1000)+0.5))/1000.0;
+circumference= ((int)((area*1000)+0.5))/1000.0;
+diameter= ((int)((diameter*1000)+0.5))/1000.0;
         
         System.out.println("the radius of the circle= " + radius);
          System.out.println("the area of the circle= " + area);
@@ -30,5 +41,11 @@ public class Program6
 //Paste console output below:
 /*
 
+enter radius: 
+2
+the radius of the circle= 2.0
+the area of the circle= 12.566
+the circumference of the circle= 12.566
+the diameter of the circle= 4.0
 
 */
